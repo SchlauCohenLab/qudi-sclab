13b12ea6097ad8299dd82f8bd2d5341156d51a13
Adrien Rousseau <adrienrousseau9@gmail.com>
2024-03-08 10:28:02 -0500

    Starting adding motor hardware and working on logic and GUI
    - Add Newport 8742 and ESP300 controller series to harware
    - Create motor logic module (need to be improved)
    - Create motor GUI

--- src/qudi/logic/motor_logic.py
+++ src/qudi/logic/motor_logic.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['TemplateLogic']
+__all__ = ['MotorLogic']
 
 from PySide2 import QtCore
 
@@ -8,80 +8,103 @@ from qudi.core.module import LogicBase
 from qudi.core.connector import Connector
 from qudi.core.statusvariable import StatusVar
 from qudi.core.configoption import ConfigOption
-from qudi.util.mutex import Mutex
-
+from qudi.util.mutex import RecursiveMutex
 
 # qudi logic measurement modules must inherit qudi.core.module.LogicBase or other logic modules.
-class TemplateLogic(LogicBase):
-    """ This is a simple template logic measurement module for qudi.
+class MotorLogic(LogicBase):
+    """ Logic module for interacting with the hardware motors.
+    This logic has the same structure as the MotorInterface but supplies additional functionality:
+        - motors can either be manipulated by index or by their names
+        - signals are generated on state changes
 
-    Example config that goes into the config file:
-
-    example_logic:
-        module.Class: 'template_logic.TemplateLogic'
+    motor_logic:
+        module.Class: 'motor_logic.MotorLogic'
         options:
-            increment_interval: 2
+            watchdog_interval: 1  # optional
+            autostart_watchdog: True  # optional
         connect:
-            template_hardware: dummy_hardware
+            motor: motor
     """
 
-    # Declare signals to send events to other modules connecting to this module
-    sigCounterUpdated = QtCore.Signal(int)  # update signal for the current integer counter value
+    # connector for one motor, if multiple motors are needed use the MotorCombinerInterfuse
+    motor = Connector(interface='MotorInterface')
 
-    # Declare static parameters that can/must be declared in the qudi configuration
-    _increment_interval = ConfigOption(name='increment_interval', default=1, missing='warn')
+    # declare config options :
+    _watchdog_interval = ConfigOption(name='watchdog_interval', default=1.0, missing='nothing')
+    _autostart_watchdog = ConfigOption(name='autostart_watchdog', default=False, missing='nothing')
 
-    # Declare status variables that are saved in the AppStatus upon deactivation of the module and
-    # are initialized to the saved value again upon activation.
-    _counter_value = StatusVar(name='counter_value', default=0)
+    # declare status variables (logic attribute) :
+    _move_absolute = StatusVar('move_absolute', True)
 
-    # Declare connectors to other logic modules or hardware modules to interact with
-    _template_hardware = Connector(name='template_hardware',
-                                   interface='TemplateInterface',
-                                   optional=True)
+    sigUpdatePosition = QtCore.Signal(dict)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._mutex = Mutex()  # Mutex for access serialization
 
-    def on_activate(self) -> None:
-        # Check if _increment_interval is not too small (lower boundary is 1.5 * trigger_time)
-        assert self._increment_interval >= 1.5 * self._template_hardware().trigger_time, \
-            'increment_interval must be >= 1.5 * <hardware trigger time>'
-        # Set up a Qt timer to send periodic signals according to _increment_interval
-        self.__timer = QtCore.QTimer(parent=self)
-        self.__timer.setInterval(1000 * self._increment_interval)  # Interval in milliseconds
-        self.__timer.setSingleShot(False)
-        # Connect timeout signal to increment slot
-        self.__timer.timeout.connect(lambda: self.add_to_counter(1), QtCore.Qt.QueuedConnection)
-        # Start timer
-        self.__timer.start()
+        self._thread_lock = RecursiveMutex()
 
-    def on_deactivate(self) -> None:
-        # Stop timer and delete
-        self.__timer.stop()
-        self.__timer.timeout.disconnect()
-        self.__timer = None
+        self._watchdog_active = False
+        self._watchdog_interval_ms = 0
+
+    def on_activate(self):
+        """ Activate module
+        """
+        self.motor_constraints = self.motor().get_constraints()
+        self._actual_position = self.motor().get_pos()
+        self._watchdog_interval_ms = int(round(self._watchdog_interval * 1000))
+
+    def on_deactivate(self):
+        """ Deactivate module
+        """
+        self._watchdog_active = False
 
     @property
-    def counter_value(self) -> int:
-        with self._mutex:
-            return self._counter_value
+    def actual_position(self):
+        """ Actual position of the motor axis.
 
-    def add_to_counter(self, value: int) -> None:
-        if value != 0:
-            with self._mutex:
-                if value > 0:
-                    hardware = self._template_hardware()
-                    for i in range(value):
-                        hardware.send_trigger()
-                        self._counter_value += 1
-                        self.sigCounterUpdated.emit(self._counter_value)
-                else:
-                    self._counter_value += value
-                    self.sigCounterUpdated.emit(self._counter_value)
+        @return dict pos: Dictionary with axis name and pos in deg
+        """
+        self._actual_position = self.motor().get_pos()
+        return self._actual_position
 
-    def reset_counter(self) -> None:
-        with self._mutex:
-            self._counter_value = 0
-            self.sigCounterUpdated.emit(self._counter_value)
+    def move(self, displacement):
+        """ Actual position of the motor axis.
+
+        @return dict pos: Dictionary with axis name and pos in deg
+        """
+        if self._move_absolute:
+            self.motor().move_abs(displacement)
+        else:
+            self.motor().move_rel(displacement)
+
+    @property
+    def velocity(self):
+        """ Actual position of the motor axis.
+
+        @return dict velocity: Dictionary with axis name and target velocity
+        """
+        return self.motor().get_velocity()
+
+    @velocity.setter
+    def velocity(self, velocity):
+        """ Actual position of the motor axis.
+
+        @param dict param_dict: Dictionary with axis name and target velocity
+        """
+        self.motor().set_velocity(velocity)
+
+    @property
+    def move_absolute(self):
+        """ Absolute or relative motor axis displacement.
+
+        @return bool : if the motor axis displacement is absolute.
+        """
+        return self._move_absolute
+
+    @move_absolute.setter
+    def move_absolute(self, move_absolute):
+        """ Actual position of the motor axis.
+
+        @param bool : if the motor axis displacement is absolute.
+        """
+        self._move_absolute = move_absolute
