4ba6ae67579353662297185e2c041ce44d8ad07d
ArthurV <varthur@mit.edu>
2025-05-12 16:33:46 -0400

    Initial completion of the IMAQ camera hardware module

--- src/qudi/hardware/camera/PCIe-1430_camera.py
+++ src/qudi/hardware/camera/PCIe-1430_camera.py
@@ -31,36 +31,13 @@ import numpy as np
 from pylablib.devices import IMAQ
 import pylablib as pll
 from qudi.core.configoption import ConfigOption
-from qudi.interface.camera_interface import CameraInterface
+from qudi.hardware_interface.camera_interface import BaseCamera
+import logging
 
+logger = logging.getLogger(__name__)
 
-
-
-
-class ReadMode(Enum):
-    FVB = 0
-    MULTI_TRACK = 1
-    RANDOM_TRACK = 2
-    SINGLE_TRACK = 3
-    IMAGE = 4
-
-
-class AcquisitionMode(Enum):
-    SNAP = 1
-    SEQUENCE = 2
-
-
-class TriggerMode(Enum):
-    INTERNAL = 0
-    EXTERNAL = 1
-    EXTERNAL_START = 6
-    EXTERNAL_EXPOSURE = 7
-    SOFTWARE_TRIGGER = 10
-    EXTERNAL_CHARGE_SHIFTING = 12
-
-
-class PCIe1430Camera(CameraInterface):
-    """ Hardware class for Andors Ixon Ultra 897
+class PCIe1430Camera(BaseCamera):
+    """ Hardware class for EV2
 
     Example config for copy-paste:
 
@@ -73,51 +50,47 @@ class PCIe1430Camera(CameraInterface):
 
     """
 
-    _dll_location = ConfigOption('dll_location', missing='warning')
-    _name = ConfigOption('cam_name', missing='error')
-
+    _dll_location = ConfigOption('dll_location', None) 
+    """"
+    the DLL is automatically added to the System32 folder,where pylablib looks for it by default. If the DLL is located elsewhere,
+    the path has to be specified. Otherzise, can be left as None. 
     """
-    Interfance with the camera requires imaq.dll, which is installed with the 
-    freely available Vision Acquisition Software, which also includes all the necessary drivers. 
-    After installation, the DLL is automatically added to the System32 folder,
-     where pylablib looks for it by default. If the DLL is located elsewhere,
-    the path can be specified using the library parameter devices/dlls/niimaq:
-    """
+    _name = ConfigOption('camera_name',default = 'cam1')
+    _default_nframes = ConfigOption('nframes', default=5000)
 
-    #pll.par["devices/dlls/niimaq"] =_dll_location
-    cam1 = IMAQ.IMAQCamera(_name)  # Put it in on_acitivate  
+    _nframe = _default_nframes
+    _live = True
+
+
 
 
     def on_activate(self):
-        """ Initialisation performed during activation of the module.
-         """
-        # self.cam.SetAcquisitionMode(1)  # single
-        # self.cam.SetTriggerMode(0)  # internal
-        # self.cam.SetCoolerMode(0)  # Returns to ambient temperature on ShutDown
-        # self.set_cooler_on_state(self._cooler_on)
-        # self.set_exposure(self._exposure)
-        # self.set_setpoint_temperature(self._temperature)
-        self.dll = cdll.LoadLibrary(self._dll_location)
-        self.dll.Initialize()
-        nx_px, ny_px = c_int(), c_int()
-        self._get_detector(nx_px, ny_px)
-        self._width, self._height = nx_px.value, ny_px.value
-        self._set_read_mode(self._read_mode)
-        self._set_trigger_mode(self._trigger_mode)
-        self._set_exposuretime(self._exposure)
-        self._set_acquisition_mode(self._acquisition_mode)
+        """ Initialisation performed during activation of the module."""
+        if self._dll_location:
+            pll.par["devices/dlls/niimaq"] = self._dll_location
+        try:
+            self.cam = IMAQ.IMAQCamera(self._name)
+            self._width, self._height = self.cam.get_detector_size()
+        except Exception as e:
+            logger.error(f"Failed to start NI IMAQ camera: {e}")
+            raise
 
     def on_deactivate(self):
         """ Deinitialisation performed during deactivation of the module.
         """
-        self.stop_acquisition() #also break the connection and so on
+        if self.cam:
+            self.cam.close()
+            self.cam = None
+            logger.info("NI IMAQ camera stopped")
+        else:
+            logger.info("Camera is not activated, nothing to stop")
 
     def get_name(self):
         """ Retrieve an identifier of the camera that the GUI can print
 
         @return string: name for the camera
         """
-        return cam1.get_device_info()
+        return self.cam.get_device_info()
 
 
     def get_size(self):
@@ -125,7 +98,7 @@ class PCIe1430Camera(CameraInterface):
 
         @return tuple: Size (width, height)
         """
-        return cam1.get_detector_size()
+        return  self._width, self._height
 
 
     def support_live_acquisition(self):
@@ -133,7 +106,7 @@ class PCIe1430Camera(CameraInterface):
 
         @return bool: True if supported, False if not
         """
-        return True
+        return self._live
 
 
     def start_live_acquisition(self):
@@ -141,28 +114,29 @@ class PCIe1430Camera(CameraInterface):
 
         @return bool: Success ?
         """
-        cam1.start_acquisition(mode='sequence', nframes=100)  # ask Adrien how it should be done here 
-        return cam1.acquisition_in_progress() 
+        self.cam.start_acquisition(mode='sequence', nframes=self._nframe) 
+        self.cam.start_acquisition() 
+        return self.cam.acquisition_in_progress() 
 
 
     def start_single_acquisition(self):
-        """ Start a single acquisition
+        """ Start a single snao acquisition
 
         @return bool: Success ?
         """
-        self.setup_acquisition(mode='snap', nframes=self._frames)
-        pass self.acquisition_in_progress() # ask Adrien how it should be done here 
-
+        self.cam.setup_acquisition(mode='snap', nframes=self._frames)
+        self.cam.start_acquisition()
+        return self.acquisition_in_progress() 
 
     def stop_acquisition(self):
         """ Stop/abort live or single acquisition
 
         @return bool: Success ?
         """
-        if cam1.acquisition_in_progress():
-            cam1.stop_acquisition()
-
-        return  not cam1.acquisition_in_progress()
+        if self.cam.acquisition_in_progress():
+            self.cam.stop_acquisition()
+            logger.debug("Acquisition aborted")
+        return  not self.acquisition_in_progress()
 
 
     def get_acquired_data(self):
@@ -172,54 +146,34 @@ class PCIe1430Camera(CameraInterface):
 
         Each pixel might be a float, integer or sub pixels
         """
-        cam1.
-        return cam1.read_newest_image()
+        return self.cam.read_newest_image()
 
 
-    def set_exposure(self, exposure):
-        """ Set the exposure time in seconds
+    def set_nframe(self, nframe):
+        """ Set the number of frames per steps
 
-        @param float exposure: desired new exposure time
+        @param float frame: desired number of frame per step
 
-        @return float: setted new exposure time
+        @return float: setted new number of frame 
         """
-        if not float(exposure):
-            self.log.warning("Give me some float")
-            return
+        if not float(nframe) or nframe <= 0:
+            self.log.warning("Give me some positive float")
+            return self._nframe
+        self._nframe = nframe
+        logger.info("Number of frames set to %s", self._nframe)
+        return self._nframe
         
-        self._frames = exposure//dt
-        pass
 
+    def get_nframe(self):
+        """ Get the number of frames per steps
 
-    def get_exposure(self):
-        """ Get the exposure time in seconds
-
-        @return float exposure time
+        @return float number of framws
         """
-        pass
-
-
-    def set_gain(self, gain):
-        """ Set the gain
-
-        @param float gain: desired new gain
-
-        @return float: new exposure gain
-        """
-        pass
-
-
-    def get_gain(self):
-        """ Get the gain
-
-        @return float: exposure gain
-        """
-        pass
-
+        return self. _nframe
 
     def get_ready_state(self):
         """ Is the camera ready for an acquisition ?
 
         @return bool: ready ?
         """
-        pass
+        return self.cam is not None
