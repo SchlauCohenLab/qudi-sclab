cd53d0e3280e3655defc2b3b05766e7da35cdc22
ArthurV <varthur@mit.edu>
2025-08-07 12:55:06 -0400

    Addition of an option to use fs

--- src/qudi/gui/actuator/delay_stage_gui.py
+++ src/qudi/gui/actuator/delay_stage_gui.py
@@ -26,6 +26,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.axis = axis
         self.actuator_logic = actuator_logic
 
+        self._unit = 'mm'
+        self.light_speed =  3e-4 #in mm/fs
+
+        self.stage_lower_limit = self.axis.step_range[0]*1e-3
+        self.stage_upper_limit = self.axis.step_range[1]*1e-3
+
+
+
 
         self.central_widget = QtWidgets.QWidget()
         self.setCentralWidget(self.central_widget)
@@ -43,7 +51,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.up_button = QtWidgets.QPushButton("▲")
         self.step_input = QtWidgets.QLineEdit("0.1")
         self.step_input.setFixedWidth(60)
-        self.step_input.setValidator(QtGui.QDoubleValidator())
         arrow_layout.addWidget(self.down_button)
         arrow_layout.addWidget(self.step_input)
         arrow_layout.addWidget(self.up_button)
@@ -61,10 +68,25 @@ class MainWindow(QtWidgets.QMainWindow):
         goto_layout = QtWidgets.QHBoxLayout()
         self.goto_input = QtWidgets.QLineEdit()
         self.goto_input.setPlaceholderText("Enter position [mm]")
+
+        validator = QtGui.QDoubleValidator(self.stage_lower_limit*1e3 , self.stage_upper_limit*1e3 , 4)  #to be a number between max and min, with a maximum of 4 decimals
+        validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
+        self.goto_input.setValidator(validator)
+
         self.goto_button = QtWidgets.QPushButton("Go")
         goto_layout.addWidget(self.goto_input)
         goto_layout.addWidget(self.goto_button)
         layout.addLayout(goto_layout)
+        
+        #T0 position
+        time_zero_layout = QtWidgets.QHBoxLayout()
+        self.time_zero_input =  QtWidgets.QLineEdit()
+        self.time_zero_input.setPlaceholderText("Enter t0 [mm]")
+        self.time_zero_input.setValidator(validator)
+        self.fs_button = QtWidgets.QPushButton("Change to fs")
+        time_zero_layout.addWidget(self.time_zero_input)
+        time_zero_layout.addWidget(self.fs_button)
+        layout.addLayout(time_zero_layout)
 
         # Movement lights
         light_layout = QtWidgets.QHBoxLayout()
@@ -101,32 +123,102 @@ class MainWindow(QtWidgets.QMainWindow):
         self.poll_timer.timeout.connect(self.pull_movement_status)
         self.poll_timer.start(200)
 
+        #Conversion to fs
+        self.fs_button.clicked.connect(self.fs_change)
+        self.time_zero_input.editingFinished.connect(self._t0_update)
+
+
+
+
+    def _step_is_valid(self, direction=None):
+        step = float(self.step_input.text())
+        try:
+            if self._unit == 'fs':
+                step *= self.light_speed / 1e3
+            else:
+                step *= 1e-3
+            if not (1e-6 <= step <= 0.001):
+                raise ValueError
+            return step
+        except ValueError:
+            QtWidgets.QMessageBox.warning(self.central_widget, "Value Error", "Step size must be between 0.001 and 1 mm (4-3000fs).")
+
+        if direction == +1:
+            self.up_timer.stop()
+        elif direction == -1:
+            self.down_timer.stop()
+            return None
+
     def move_stage(self, direction):
-        step = float(self.step_input.text())/1e3
-        self.actuator_logic().set_delay_rel({self.axis.name: direction * step})
+        step = self._step_is_valid(direction)
+        if step is None:
+            return
+        else:
+            self.actuator_logic().set_delay_rel({self.axis.name: direction * step})
+
 
     def go_max(self):
-        max_pos = self.axis.step_range[1]/1e3
-        self.actuator_logic().set_delay_abs({self.axis.name:max_pos})
+        self.actuator_logic().set_delay_abs({self.axis.name:self.stage_upper_limit})
 
 
     def go_min(self):
-        min_pos = self.axis.step_range[0]/1e3
-        self.actuator_logic().set_delay_abs({self.axis.name:min_pos})
-
+        self.actuator_logic().set_delay_abs({self.axis.name:self.stage_lower_limit})
 
 
     def goto_position(self):
-        value = float(self.goto_input.text())/1e3
-        if value >  self.axis.step_range[1]/1e3 or value <  self.axis.step_range[0]/1e3:
-            self.log.error(f"Value out of Range") 
+        if self._unit == "mm":
+            value = float(self.goto_input.text())/1e3
+        else:
+            value = (float(self.goto_input.text())*self.light_speed + self._t0)/1e3 
+        if value >  self.stage_upper_limit or value <  self.stage_lower_limit:
+                QtWidgets.QMessageBox.warning(self.central_widget, "Value Error", "Value out of delay stage range.") 
+                return
         else:
             self.actuator_logic().set_delay_abs({self.axis.name: value})
 
+
     def update_position(self):
-        pos = self.actuator_logic().get_delay()[self.axis.name]
-        self.position_display.setText(f"Position: {pos*1e3:.4f} mm")
-        self._last_position = pos
+        if self._unit == "mm":
+            _pos =  self.actuator_logic().get_delay()[self.axis.name]
+            self.position_display.setText(f"Position: {_pos*1e3:.4f} "+ self._unit)
+        else:
+            _pos = self.actuator_logic().get_delay()[self.axis.name] #in m
+            _pos_fs = (_pos*1e3 - self._t0) / self.light_speed #in fs
+            self.position_display.setText(f"Position: {_pos_fs:.0f} "+ self._unit)
+        self._last_position = _pos
+
+    def fs_change(self):
+        if self.time_zero_input.text().strip() == "":
+            QtWidgets.QMessageBox.warning(self.central_widget, "Input Error", "Please enter a value for t₀.")
+            return
+        else: 
+            if not (self.stage_upper_limit*1e3 > float(self.time_zero_input.text()) >  self.stage_lower_limit*1e3):
+                QtWidgets.QMessageBox.warning(self.central_widget, "Value Error", " t0 value is out of range.") 
+                return
+            elif self.fs_button.text() == "Change to fs":
+                self._unit = 'fs' 
+                self._t0 = float(self.time_zero_input.text())
+                self.goto_input.setPlaceholderText("Enter position [fs]")
+                self.step_input.setText("100")
+                self.fs_button.setText('Change to mm')
+                self.update_position()
+
+            elif self.fs_button.text() == "Change to mm":
+                self._unit = 'mm' 
+                self.goto_input.setPlaceholderText("Enter position [mm]")
+                self.step_input.setText("0.1")
+                self.fs_button.setText('Change to fs')
+                self.update_position()
+    
+    def _t0_update(self):
+            if not (self.stage_upper_limit*1e3 > float(self.time_zero_input.text()) >  self.stage_lower_limit*1e3):
+                QtWidgets.QMessageBox.warning(self.central_widget, "Value Error", " t0 value is out of range.") 
+                return
+            elif self.fs_button.text() == "Change to mm":
+                self._t0 = float(self.time_zero_input.text())
+                self.update_position()
+
+
 
     def pull_movement_status(self):
         current = self.actuator_logic().get_delay()[self.axis.name]
